c57cfd2e00b3a6b6cc410d04cbaab5496445c087
Andy Plymate <96448051+Plymatea@users.noreply.github.com>
2022-05-06 12:07:19 -0700

    add error handling middleware and asyncHandler.

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express()
 
-app.use('/api/member', require('./routes/memberRoutes'))
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded( {extended: false} ))
 
+
+// Member Routes
+app.use('/api/member', require('./routes/memberRoutes'))
+app.use(errorHandler)
+
+// Start listener on Port
 app.listen(port, () => console.log(`Server started on port: ${port}`))
 
