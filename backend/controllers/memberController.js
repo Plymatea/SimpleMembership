73bb535375f8560a57eac11005536ab68d2a7627
Andy Plymate <96448051+Plymatea@users.noreply.github.com>
2022-05-06 11:47:10 -0700

    dry code in memberRoutes.js

--- backend/controllers/memberController.js
+++ backend/controllers/memberController.js
@@ -1,14 +1,30 @@
+//@desc  Get Members
+// @route GET /api/goals
+// @access private
+
 const getMembers = (req, res) => {
   res.status(200).json({"message": 'get members'})
 }
+
+//@desc  Create Members
+// @route POST /api/goals
+// @access private
 
 const createMember = (req, res) => {
   res.status(200).json({"message": 'Create members'})
 }
 
+//@desc  Edit Specific Member
+// @route PUT /api/goals/:id
+// @access private
+
 const editMember = (req, res) => {
   res.status(200).json({"message": `update member ${req.params.id}`})
 }
+
+//@desc  Delete Specific Member
+// @route Delete /api/goals/:id
+// @access private
 
 const deleteMember = (req, res) => {
   res.status(200).json({"message": `delete member ${req.params.id}`})
--- backend/routes/memberRoutes.js
+++ backend/routes/memberRoutes.js
@@ -2,13 +2,8 @@ const express = require('express')
 const router  = express.Router()
 const { getMembers, createMember, editMember, deleteMember} = require('../controllers/memberController')
 
+router.route('/').get(getMembers).post(createMember)
+router.route('/:id').put(editMember).delete(deleteMember)
 
-router.get('/', getMembers)
-
-router.post('/', createMember)
-
-router.put('/:id', editMember)
-
-router.delete('/:id', deleteMember)
 
 module.exports = router
