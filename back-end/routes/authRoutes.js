f063d49560be9de2aa3a534a579e9551b540651d
Andy Plymate <96448051+Plymatea@users.noreply.github.com>
2022-11-21 14:49:08 -0800

    add passportStrategy index to dry and modularize

--- back-end/routes/authRoutes.js
+++ back-end/routes/authRoutes.js
@@ -2,18 +2,15 @@ const express = require('express')
 const router  = express.Router()
 const passport = require('passport')
 
-// Below function to DRY up CODE below if it works right
-
-// function NewlyCreatedUser(res) {
-//   if (res.req.user.createdAt === res.req.user.updatedAt) {
-//     res.redirect('http://localhost:3000/editmemberform')
-//   } else {
-//     res.redirect('http://localhost:3000/member')
-//   }
-// }
+function NewlyCreatedUser(res) {
+  if (res.req.user.createdAt === res.req.user.updatedAt) {
+    res.redirect('http://localhost:3000/editmemberform')
+  } else {
+    res.redirect('http://localhost:3000/member')
+  }
+}
 
 //  /api/auth/status
-
 router.get("/status", (req, res) => {
   return req.user ? res.send(req.user) : res.sendStatus(401);
 });
@@ -21,25 +18,16 @@ router.get("/status", (req, res) => {
 //  /api/auth/discord
 router.get("/discord", passport.authenticate('discord', { prompt: "none" }));
 
-//  /api/auth/discord/redirect
 // This is hacky. But is the user "createdAt" && "updatedAt" fields are equal it will redirect to the edit member page. The idea here is that the only time these should be equal is when the user is first created. All other times these should be different thus, redirect to the member page instead. There must be a better way, but this works for now. 
 router.get("/discord/redirect", passport.authenticate('discord'), (req, res) => {
-  if (res.req.user.createdAt === res.req.user.updatedAt) {
-    res.redirect('http://localhost:3000/editmemberform')
-  } else {
-    res.redirect('http://localhost:3000/member')
-  }
+  NewlyCreatedUser(res)
 });
 
 //  /api/auth/google
 router.get("/google", passport.authenticate('google', { scope: [ "email", "profile"] }));
 
 router.get("/google/redirect", passport.authenticate('google'), (req, res) => {
-  if (res.req.user.createdAt === res.req.user.updatedAt) {
-    res.redirect('http://localhost:3000/editmemberform')
-  } else {
-    res.redirect('http://localhost:3000/member')
-  }
+  NewlyCreatedUser(res)
 });
 
 module.exports = router
