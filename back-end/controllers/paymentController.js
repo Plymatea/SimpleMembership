bf9675a35df2c1af99f05df5ce28a648e0536f30
Andy Plymate <andy.plymate@gmail.com>
2022-12-30 23:04:54 -0800

    create dynamic api point for stripe checkout

--- back-end/controllers/paymentController.js
+++ back-end/controllers/paymentController.js
@@ -1,4 +1,3 @@
-const { request } = require('express')
 const stripe = require ('stripe')('sk_test_51M9FkHLmxGS25FUi1Vj5tUZSfTkvoRBCNr8kTEONrxzrccvaDZ1m7an1loCbE2PefMCDrclmKflcKV3ZLGl7ZITM00sO3XxiJt')
 const asyncHandler = require ('express-async-handler')
 const MY_DOMAIN = 'http://localhost:3000/payment'
@@ -8,16 +7,16 @@ const createCheckoutSession = asyncHandler(async (req, res) => {
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1MKDnALmxGS25FUiwQJrX1hT', 
+        price: req.body.stripePriceId,
         quantity: 1,
       },
     ],
-    mode: 'subscription',
+    //if any of the line_items are subscriptions this must be "subscription"
+    mode: req.body.stripePaymentMode, 
     success_url: `${MY_DOMAIN}?success=true`,
     cancel_url: `${MY_DOMAIN}?canceled=true`,
   });
-  console.log("Session", session.url);
-  res.redirect(303, MY_DOMAIN);
+  res.status(303).json({link: session.url});
 })
 
 module.exports = createCheckoutSession
